ad34faee46970e116b8801c7ecbc913c229404af
sevc4ik <31854320+sevc4ik@users.noreply.github.com>
2022-02-13 02:26:45 +0300

    feat: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stk = [];
+
+  str.split('').forEach(Element => {
+    bracketsConfig.find(double => double[0] === stk[stk.length - 1] && double[1] === Element) ? stk.pop() : stk.push(Element)
+  }
+  );
+  return !stk.length;
 }
